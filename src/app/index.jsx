de46dddcc3c3013185a73f68a51a27f7df69114c
Vitor Gagliani <93485462+VitorGagliani@users.noreply.github.com>
2026-09-22 23:02:57 -0300

    00

---
+++ src/app/index.jsx
@@ -0,0 +1,92 @@
+import { Image, View, Text, StyleSheet, Pressable } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Image source={require("./pomodoro.png")} />
+
+
+      <View style={styles.actions}>
+        <View style={styles.context}>
+          <Pressable style={styles.contextTextActive}>
+            <Text style={styles.contextText}>Foco</Text>
+          </Pressable>
+          <Pressable>
+            <Text style={styles.contextText}>Pausa curta</Text>
+          </Pressable>
+          <Pressable>
+            <Text style={styles.contextText}>Pausa longa</Text>
+          </Pressable>
+        </View>
+        <Text style={styles.timer}>25:00</Text>
+        <Pressable style={styles.buttom}>
+          <Text style={styles.buttonText}>Começar</Text>
+        </Pressable>
+      </View>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>
+          Projeto fictício e sem fins comerciais. Desenvolvido por Alura.{" "}
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#021123",
+    gap: 40,
+  },
+  actions: {
+    padding: 24,
+    backgroundColor: "#14448080",
+    width: `80%`,
+    borderRadius: 32,
+    borderWidth: 2,
+    borderColor: "#144480",
+    gap: 32,
+  },
+  context:{
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignContent: 'center'
+  }, 
+  contextTextActive:{
+    backgroundColor: '#144480',
+    borderRadius: 8
+  },
+  contextText:{
+    fontSize: 12.5,
+    color: '#FFF',
+    padding: 8
+  },
+  timer: {
+    fontSize: 54,
+    color: "#FFF",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  buttom: {
+    backgroundColor: "#B872FF",
+    borderRadius: 32,
+    padding: 8,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "#021123",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  footer: {
+    width: "80%",
+  },
+  footerText: {
+    color: "#98A0A8",
+    textAlign: "center",
+    fontSize: 12.5,
+  },
+});
